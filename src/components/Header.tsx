db1fdb9eba8fbb6c203611857e7a7e93a99a841f
VBzDev <zkcromossomu@gmail.com>
2021-05-05 01:57:26 -0300

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,27 @@
 import react from 'react';
-import logoImg from "../public/images/youtube-logo-7-2.png";
+import iconLogo from "../public/images/youtube-logo-7-2.png";
 import Menu from "./icons/menu"
+import iconSearch from "../public/images/search.png";
 
 import "../styles/components/header.css"
 
 export default function headers() {
     return (
         <div className="container">
-            <div className="logo">
+            <div className="leftHeader">
                 <Menu/>
-                <img className="logoImg" src={logoImg} />
+                <img className="iconLogo" src={iconLogo} />
                 Youtube
             </div>
 
-            <div className="pesquisa">
-                pesquisa
+            <div className="centerHeader">
+                <input placeholder="Pesquisar" className="search"type="text"/>
+                <button className="buttonSearch" >
+                    <img className="iconSearch" src={iconSearch} />
+                </button>
             </div>
 
-            <div className="conta">
+            <div className="rightHeader">
                 conta
             </div>
         </div>
