f303039ee89649f35cea16bf20532d3144bb6555
VBzDev <zkcromossomu@gmail.com>
2021-05-20 12:03:57 -0300

    Ajuste Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
 import React, { useState } from 'react';
 import iconLogo from "../public/images/youtube-logo-7-2.png";
 import iconSearch from "../public/images/search.png";
-import { faVideo, faBell, faTh } from "@fortawesome/free-solid-svg-icons"
-=======
-/* eslint-disable */
-import React, { useState } from 'react';
-import iconLogo from "../public/images/youtube-logo-7-2.png";
-import Menu from "./icons/menu"
 import { faVideo, faBell, faTh, faSearch } from "@fortawesome/free-solid-svg-icons"
->>>>>>> 969c53c92ceb6a74e832a7e24a0ad5e5d2c0ccb7
 import imgConta from "../public/images/lucasinutil.png"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import "../styles/components/header.css"
@@ -17,7 +9,7 @@ import PerfilCard from '../components/PerfilCard'
 
 export default function Headers() {
 
-
+    const [showPerfilCard, setShowPerfilCard] = useState(false)
 
     return (
         <div className="container">
